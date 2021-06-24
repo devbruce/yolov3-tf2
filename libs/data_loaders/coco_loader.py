2ea7b3ccee86040ed35be450aee2dfb287f8363c
devbruce <bruce93k@gmail.com>
2021-06-24 15:32:47 +0900

    Modify coco_loader.py

--- libs/data_loaders/coco_loader.py
+++ libs/data_loaders/coco_loader.py
@@ -18,13 +18,18 @@ class CocoDataLoader:
         self.project_name = project_name
         self.batch_size = batch_size
         self.input_size = input_size
+        
+        self.dataset_dir = os.path.join(ProjectPath.DATASETS_DIR.value, self.project_name)
+        self.imgs_dir = os.path.join(self.dataset_dir, 'imgs', self.stage)
+        self.coco_ann_path = os.path.join(self.dataset_dir, 'labels', f'{self.stage}.json')
+        self.anns = self._load_anns()
     
     def __iter__(self):
         return _CocoDataIter(
+            anns=self.anns,
             stage=self.stage,
             shuffle=self.shuffle,
             aug=self.aug,
-            project_name=self.project_name,
             batch_size=self.batch_size,
             input_size=self.input_size
         )
@@ -32,27 +37,55 @@ class CocoDataLoader:
     def __repr__(self):
         repr_form = '* project_name: {}\n- stage: {}\n- shuffle={}\n- aug={}\n- batch_size={}\n- input_size={}'
         return repr_form.format(self.project_name, self.stage, self.shuffle, self.aug, self.batch_size, self.input_size)
+    
+    def _load_anns(self):
+        anns = list()
+        coco_ann_json = json.load(open(self.coco_ann_path))
+        coco_imgs = coco_ann_json['images']
+        coco_anns = coco_ann_json['annotations']
+        for coco_img in tqdm.tqdm(coco_imgs, total=len(coco_imgs), desc=f'[{self.stage}]Loading Dataset'):
+            img_id = coco_img['id']
+            fname = coco_img['file_name']
+            img_path = os.path.join(self.imgs_dir, fname)
+            
+            ann = dict()
+            ann['image_path'] = img_path
+            ann['annotations'] = list()
+            
+            for coco_ann in coco_anns:
+                if coco_ann['image_id'] == img_id:
+                    cls_idx = coco_ann['category_id'] - 1  # !Warning
+                    pts = coco_ann['bbox']
+                    left, top, width, height = pts
+                    right = left + width
+                    bottom = top + height
+                    
+                    ann_info = dict()
+                    ann_info['bbox'] = [left, top, right, bottom]
+                    ann_info['class_index'] = cls_idx
+                    ann['annotations'].append(ann_info)
+                    
+            anns.append(ann)
+        return anns
 
 
 class _CocoDataIter:
-    def __init__(self, stage, shuffle=False, aug=False, project_name=cfg.project_name, batch_size=cfg.batch_size, input_size=cfg.input_size):
+    def __init__(self, anns, stage, shuffle=False, aug=False, batch_size=cfg.batch_size, input_size=cfg.input_size):
+        self.anns = anns
         self.stage = stage
         self.shuffle = shuffle
         self.aug = aug
-        self.project_name = project_name
         self.batch_size = batch_size
         self.input_size = input_size
         
-        self.dataset_dir = os.path.join(ProjectPath.DATASETS_DIR.value, self.project_name)
-        self.imgs_dir = os.path.join(self.dataset_dir, 'imgs', self.stage)
-        self.coco_ann_path = os.path.join(self.dataset_dir, 'labels', f'{self.stage}.json')
-        
-        self.anns = self._load_anns()
         self.num_imgs = len(self.anns)
         self.num_batchs = int(np.ceil(self.num_imgs / self.batch_size))
         
         self.idx = 0
         self.batch_idx = 0
+        
+        if self.shuffle:
+            np.random.shuffle(self.anns)
         
     def __len__(self):
         return self.num_batchs
@@ -113,36 +146,3 @@ class _CocoDataIter:
         self.batch_idx += 1
         imgs = np.stack(img_arr_list).astype(np.float32)
         return imgs, labels_list
-    
-    def _load_anns(self):
-        anns = list()
-        coco_ann_json = json.load(open(self.coco_ann_path))
-        coco_imgs = coco_ann_json['images']
-        coco_anns = coco_ann_json['annotations']
-        for coco_img in tqdm.tqdm(coco_imgs, total=len(coco_imgs), desc=f'[{self.stage}]Loading Dataset'):
-            img_id = coco_img['id']
-            fname = coco_img['file_name']
-            img_path = os.path.join(self.imgs_dir, fname)
-            
-            ann = dict()
-            ann['image_path'] = img_path
-            ann['annotations'] = list()
-            
-            for coco_ann in coco_anns:
-                if coco_ann['image_id'] == img_id:
-                    cls_idx = coco_ann['category_id'] - 1  # !Warning
-                    pts = coco_ann['bbox']
-                    left, top, width, height = pts
-                    right = left + width
-                    bottom = top + height
-                    
-                    ann_info = dict()
-                    ann_info['bbox'] = [left, top, right, bottom]
-                    ann_info['class_index'] = cls_idx
-                    ann['annotations'].append(ann_info)
-                    
-            anns.append(ann)
-            
-        if self.shuffle:
-            np.random.shuffle(anns)
-        return anns
